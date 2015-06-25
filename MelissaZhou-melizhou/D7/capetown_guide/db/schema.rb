ee5257a86d27301d5b7fa3bbca4714589c1a4398
MELIZHOU <MZHOU595@GMAIL.COM>
2015-06-25 19:31:16 +0200

    D9 homework

--- MelissaZhou-melizhou/D7/capetown_guide/db/schema.rb
+++ MelissaZhou-melizhou/D7/capetown_guide/db/schema.rb
@@ -11,22 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625171256) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "place_id"
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.string   "title"
-    t.string   "picture"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
