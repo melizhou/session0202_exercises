1ecbd798f21b08b6a6975285d19d50a2016c8532
MELIZHOU <MZHOU595@GMAIL.COM>
2015-06-27 20:14:43 +0200

    Remove git from fill_in_the_blanks

--- MelissaZhou-melizhou/D7/capetown_guide/db/schema.rb
+++ MelissaZhou-melizhou/D7/capetown_guide/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150626134116) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
